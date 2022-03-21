4fe74da5b9220bfa5250473a092676876f528855
Dongsik <ads226@naver.com>
2022-03-21 12:15:16 +0900

    UIDragHandler
    ver.1.0.0

--- components/UIDragHandler.js
+++ components/UIDragHandler.js
@@ -24,16 +24,19 @@ class UIDragHandler extends HTMLElement {
 			}
 		`;
 
+		this.onPress = this.onPress.bind(this);
+		this.onMove = this.onMove.bind(this);
+		this.onRelease = this.onRelease.bind(this);
+		this.onTransitionEnd = this.onTransitionEnd.bind(this);
+
 		this.target = '';
 
 		this.handle = document.createElement('div');
 		this.handle.id = 'handle';
-		this.handle.addEventListener('mousedown', this.onPress.bind(this));
-		this.handle.addEventListener('mousemove', this.onMove.bind(this));
-		this.handle.addEventListener('mouseup', this.onRelease.bind(this));
+		this.handle.addEventListener('mousedown', this.onPress);
 
 		const slot = document.createElement('slot');
-		
+
 		shadow.append(style, this.handle, slot);
 	}
 
@@ -41,41 +44,69 @@ class UIDragHandler extends HTMLElement {
 		e.stopImmediatePropagation;
 		e.stopPropagation;
 
-		if (this.isPress) return;
-		this.isPress = true;
-		
 		this.handle.classList.add('press');
-		this.prevX = e.pageX;
-		this.prevY = e.pageY;
+
+		this.prevX = e.clientX;
+		this.prevY = e.clientY;
+
+		window.addEventListener('mousemove', this.onMove);
+		window.addEventListener('mouseup', this.onRelease);
+		window.addEventListener('mouseleave', this.onRelease);
 	}
 
 	onMove(e) {
-		e.stopImmediatePropagation;
-		e.stopPropagation;
+		this.target.style.left = this.target.offsetLeft + (e.clientX - this.prevX) + 'px';
+		this.target.style.top = this.target.offsetTop + (e.clientY - this.prevY) + 'px';
 
-		if (!this.isPress) return;
-
-		this.target.style.left = this.target.clientLeft + (e.pageX - this.prevX) + 'px';
-		this.target.style.top = this.target.clientTop + (e.pageY - this.prevY) + 'px';
-
-		this.prevX = e.pageX;
-		this.prevY = e.pageY;
+		this.prevX = e.clientX;
+		this.prevY = e.clientY;
 	}
 
 	onRelease(e) {
-		e.stopImmediatePropagation;
-		e.stopPropagation;
-
-		if (!this.isPress) return;
-		this.isPress = false;
+		window.removeEventListener('mousemove', this.onMove);
+		window.removeEventListener('mouseup', this.onRelease);
+		window.removeEventListener('mouseleave', this.onRelease);
 
 		this.handle.classList.remove('press');
+
+		let tX = -1;
+		let tY = -1;
+		let parent = this.target.offsetParent;
+		let right = parent.offsetWidth - this.target.offsetWidth;
+		let bottom = parent.offsetHeight - this.target.offsetHeight;
+
+		if (this.target.offsetLeft < 0) tX = 0;
+		if (this.target.offsetLeft > right) tX = right;
+		if (this.target.offsetTop < 0) tY = 0;
+		if (this.target.offsetTop > bottom) tY = bottom;
+
+		if (!~tX && !~tY) return;
+
+		if (!~tX) tX = this.target.offsetLeft;
+		if (!~tY) tY = this.target.offsetTop;
+
+		this.prevTransition = this.target.style.transition;
+		this.target.style.transition = '0.25s';
+		this.target.addEventListener('transitionend', this.onTransitionEnd);
+
+		setTimeout(() => {
+			this.target.style.left = 'max(0px, min(' + tX + 'px, ' + this.target.offsetLeft + 'px))';
+			this.target.style.top = 'max(0px, min(' + tY + 'px, ' + this.target.offsetTop + 'px))';
+		}, 0);
+	}
+
+	onTransitionEnd() {
+		this.target.removeEventListener('transitionend', this.onTransitionEnd);
+		this.target.style.transition = this.prevTransition;
+
+		this.target.style.left = this.target.offsetLeft + 'px';
+		this.target.style.top = this.target.offsetTop + 'px';
 	}
 
 	connectedCallback() {
 		if (!this.target) this.setTarget(this);
 	}
-	
+
 	disconnectedCallback() {}
 
 	static get observedAttributes() {
@@ -92,8 +123,8 @@ class UIDragHandler extends HTMLElement {
 	setTarget(target) {
 		const targetElement = this.getTarget(target);
 		targetElement.prevPosition = targetElement.style.position || 'static';
-		targetElement.style.position = 'fixed';
-		
+		targetElement.style.position = 'absolute';
+
 		this.target = targetElement;
 	}
 
